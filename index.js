59057b23d21aa4ff74c63dcfcc5c8dc920466f24
Osbaldo Martinez Jr <martinez.osbaldo@gmail.com>
2017-11-12 20:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,9 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
   return 'No, I must disagree'
 }
+BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
